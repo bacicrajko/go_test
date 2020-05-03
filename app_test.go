19eab402e1c1c23b6f3b0bb28ca3570e15025301
rajko bacic <rajko@emakina.rs>
2020-05-03 17:19:08 +0200

    first

---
+++ app_test.go
@@ -0,0 +1,10 @@
+package go_pipeline
+
+import (
+	"github.com/magiconair/properties/assert"
+	"testing"
+)
+
+func TestAddition (t *testing.T){
+	assert.Equal(t, 1,1)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bacicrajko/pipeline_test
+
+go 1.14
+
+require github.com/magiconair/properties v1.8.1
